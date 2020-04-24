d94ba500824d0053e630ee463b99f153f430b7d0
Mylov Vasily <zarkaria.dia@gmail.com>
2020-04-25 02:55:19 +0500

    Created solution base

---
+++ Hexapod.EntryPoint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hexapod.EntryPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
